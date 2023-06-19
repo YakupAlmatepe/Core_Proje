616d032507e289a486d26304289637f1fb7accf3
Yakup Almatepe <almatepe.yakup@gmail.com>
2023-06-20 01:54:17 +0300

    20.06.2023#

---
+++ Core_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeaderPartial() //header bölümünü partial ile headPartial.schtml e taşıdık
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
